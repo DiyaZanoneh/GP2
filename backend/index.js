2663ca3abf402eb4b17cd16ee252ea607e9651d8
DiyaAbuZanouneh <deeyazanoneh@gmail.com>
2025-03-17 16:00:45 +0300

    change on index.js file

--- backend/index.js
+++ backend/index.js
@@ -26,10 +26,8 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ||3000;
 app.listen(PORT , ()=>{
     console.log(`Server running at port ${PORT}`);
 });
-
 
