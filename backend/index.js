350b391658685e28ec1c07dfb2e56f89e46ec4d0
DiyaAbuZanouneh <deeyazanoneh@gmail.com>
2025-03-17 16:55:41 +0300

    add utils db.js for connection with database and models folder

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
 dotenv.config({});
 
 
@@ -28,6 +29,7 @@ app.use(cors(corsOptions));
 
 const PORT = process.env.PORT ||3000;
 app.listen(PORT , ()=>{
+    connectDB();
     console.log(`Server running at port ${PORT}`);
 });
 
