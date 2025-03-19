78841e50fe63e750aea57e5c97e7be40ebd17ac2
DiyaAbuZanouneh <deeyazanoneh@gmail.com>
2025-03-19 19:42:39 +0300

    add user controller and route + add middleware

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRouter from "./routes/user.route.js";
 dotenv.config({});
 
 
@@ -28,6 +29,12 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
+
+//api's
+app.use("/api/v1/user" , userRouter);
+
+
+
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
