6ce1040b495768495afeb5f655a33511d9f6901f
DiyaAbuZanouneh <deeyazanoneh@gmail.com>
2025-03-22 00:22:26 +0300

    update on company route,model,controller

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRouter from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
 dotenv.config({});
 
 
@@ -32,6 +33,7 @@ const PORT = process.env.PORT || 3000;
 
 //api's
 app.use("/api/v1/user" , userRouter);
+app.use("/api/v1/company", companyRoute);
 
 
 
--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -3,14 +3,14 @@ import mongoose from "mongoose";
 const companySchema = new mongoose.Schema({
     name:{
         type:String,
-        required:true
+        required:true,
+        unique:true
     },
     description:{
         type:String
     },
     website:{
-        type:String,
-        required:true
+        type:String
     },
     location:{
         type:String
