903899e171372ca8f9369019b27bf3c9457f99cf
DiyaAbuZanouneh <deeyazanoneh@gmail.com>
2025-03-22 20:03:57 +0300

    add application controller,route and update on job model

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -1,48 +1,48 @@
 import mongoose from "mongoose";
 
-const jobSchema= new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
+const jobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
     },
-    description:{
-        type:String,
-        required:true
+    description: {
+        type: String,
+        required: true
     },
-    salary:{
-        type:Number,
-        required:true
+    salary: {
+        type: Number,
+        required: true
     },
-    experienceLevel:{
-        type:Number,
-        required:true
-    },   
-    location:{
-        type:String,
-        required:true
+    experienceLevel: {
+        type: Number,
+        required: true
     },
-    jobType:{
-        type:String,
-        required:true
+    location: {
+        type: String,
+        required: true
     },
-    position:{
-        type:Number,
-        required:true
+    jobType: {
+        type: String,
+        required: true
     },
-    company:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Company',
-        required:true
+    position: {
+        type: Number,
+        required: true
     },
-    created_by:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required:true
+    company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company',
+        required: true
     },
-    application:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Application',
-    }
-},{timestamps:true});
+    created_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    applications: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Application'
+    }]
+}, {timestamps: true});
 
-export const Job = mongoose.model("Job" , jobSchema);
+export const Job = mongoose.model("Job", jobSchema);
