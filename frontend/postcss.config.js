bf449db7898403b18a06227f5087f11b1ca2c20e
DiyaAbuZanouneh <deeyazanoneh@gmail.com>
2025-04-20 13:44:03 +0300

    update packages and add folders

---
+++ frontend/postcss.config.js
@@ -0,0 +1,13 @@
+export default {
+<<<<<<< HEAD:frontend/my-app/postcss.config.js
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+=======
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+>>>>>>> 2004b8b (update on packages and add folders):frontend/postcss.config.js
+}
